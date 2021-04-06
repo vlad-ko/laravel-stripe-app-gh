e46a4d47ca6614e55ef981bf9ac10750e438c117
vlad kobilansky <vlad.kobilansky@gmail.com>
2021-04-06 19:04:26 -0400

    adding silly things like a demo service

---
+++ app/Services/DemoService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+/**
+ * Used to create a bunch of random demo functions
+ */
+class DemoService {
+
+	public function uncoveredFunctionWhichReturnsSeven() {
+        return 7;
+    }
+
+}
