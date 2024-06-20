dcba7e5924d5d5eb6b9bb1d88dcf0f19baa579d3
vlad kobilansky <vlad.kobilansky@gmail.com>
2024-06-19 21:32:51 -0400

    adding a blank model

--- app/Models/ImportantOne.php
+++ app/Models/ImportantOne.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ImportantOne extends Model
 {
-    use HasFactory;
 
     public function brandNewFunctionHasNoTest() {
         if ( $_COOKIE['codecov'] === '' ) {
