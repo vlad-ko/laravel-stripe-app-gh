637c7072f3875d4402c2fbab0bf57a79285d212f
vlad kobilansky <vlad.kobilansky@gmail.com>
2024-06-19 21:27:21 -0400

    fixing the model name

--- app/Models/ImportantOne.php
+++ app/Models/ImportantOne.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Charge extends Model
+class ImportantOne extends Model
 {
     use HasFactory;
 
